90d43dd3e4ced7c99ec0827db4ac2cc800fc05b6
mariapolyantseva <mariapolyantseva@yandex.ru>
2021-07-16 01:35:37 +0300

    instagram

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-	def after_sign_in_path_for(user) # resource was as paramentr 
+	def after_sign_in_path_for(user)
 		user_path(user)	
   	end
 
-  	def after_sign_out_path_for(resource) # resource was as paramentr
+  	def after_sign_out_path_for(resource)
   		new_user_session_path
   	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,23 +10,3 @@ class UsersController < ApplicationController
 	end
 
 end
-=begin
-	def edit
-		@user = User.find(params[:id])
-	end
-
-	def update
-		@user = User.find(params[:id])
-		if user.update(user_update_params)
-			redirect_to user_path(@user)
-		else
-			render :edit
-		end
-	end
-
-	private
-
-	def user_update_params
-		params.require(:user).permit(:email)
-	end
-=end
